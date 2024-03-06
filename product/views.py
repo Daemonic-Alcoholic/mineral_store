b8d56290920ce1768e14145a66328e086cc04aa5
Daemonic Alcoholic <missed.semicolon1101@gmail.com>
2024-03-06 22:47:48 +0300

    [1.13] Updated views and urls

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def product_detail(request, pk):
+    template_name = 'product/product_detail.html'
+    context = {}
+    return render(request, template_name, context)
+
+
+def product_list(request):
+    template_name = 'product/product_list.html'
+    context = {}
+    return render(request, template_name, context)
