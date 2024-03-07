9e8e9e8ef9c9c6bb7e667d04e296e5c0c345199b
Daemonic Alcoholic <missed.semicolon1101@gmail.com>
2024-03-07 22:59:35 +0300

    [1.13] Added new app / page for shipments detail

--- mineral_store/urls.py
+++ mineral_store/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
-    path('product/', include('product.urls'))
+    path('product/', include('product.urls')),
+    path('shipment/', include('shipment.urls')),
 ]
