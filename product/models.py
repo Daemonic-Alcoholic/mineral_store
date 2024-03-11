c5462a5ad545034ab26201e022d6e3a362494b85
Daemonic Alcoholic <missed.semicolon1101@gmail.com>
2024-03-11 18:17:09 +0300

    [1.14] Added first models for products, types

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=40)
+    price = models.IntegerField(verbose_name="Price")
+    description = models.TextField(verbose_name="Description")
+    image = models.ImageField(verbose_name="Image")
+    type_id = models.OneToOneField("product.Type", on_delete=models.CASCADE)
+
+
+class Type(models.Model):
+    type_name = models.CharField(verbose_name="Type Name", max_length=20)
